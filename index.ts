e317f2a9577577c6f9952670814c7ab9c9fce23e
almahdy-byte <mohamedalmahdy02@gmail.com>
2026-04-10 15:03:24 +0200

    redploy@7

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Application } from 'express'
+import * as dotenv from 'dotenv';
+import { bootstrap } from './src/app.controller';
+dotenv.config()
+const app: Application = express()
+
+
+const port: number = Number(process.env.PORT);
+
+
+
+bootstrap(app)
+app.listen(port)
+
+export default app
